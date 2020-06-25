0961f45702638a5ee5662fd146532dc7d14fe9b5
IgorYavorskiy <thorvin.mail@gmail.com>
2020-06-25 21:21:44 +0300

    js

---
+++ js/script.js
@@ -0,0 +1,40 @@
+var backgroundImages = [
+"../img/icecream-slider-1.png",
+"../img/icecream-slider-2.png",
+"../img/icecream-slider-3.png"
+];
+var feedbackLink = document.querySelector(".feedback");
+var feedbackPopup = document.querySelector(".modal-feedback");
+var overlay = document.querySelector(".overlay");
+var modalClose = feedbackPopup.querySelector(".modal-close");
+var sliderButtons = document.querySelectorAll(".checkbox-button");
+
+feedbackLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    feedbackPopup.classList.add("modal-show");
+    overlay.classList.add("modal-show");
+});
+
+modalClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    feedbackPopup.classList.remove("modal-show");
+    overlay.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        feedbackPopup.classList.remove("modal-show");
+        overlay.classList.remove("modal-show");
+    }  
+});
+
+var addSliderEventHandler = function (bgImage, sliderButton) {
+    sliderButton.addEventListener('click', function () {
+        document.body.style.backgroundImage = 'url(bgImage)';
+    });
+};
+
+for (var i = 0; i < backgroundImages.length; i++) {
+    addSliderEventHandler(backgroundImages[i], sliderButtons[i]);
+}
